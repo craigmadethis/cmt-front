a447c6241262340dd1f6b6b8593a7f0208cd2290
Craig Shewry <craigshewry@gmail.com>
2022-02-06 14:51:26 +0000

    font weight

--- components/BlogSidebar.jsx
+++ components/BlogSidebar.jsx
@@ -3,7 +3,7 @@ const PostSidebar = ( {categories} ) => {
   return (
       <sidebar className="col-span-4">
       <div className="flex-col w-full justify-end text-right">
-        <h1 className="w-full text-right text-h1 font-jost py-2 ">Lost?</h1>
+        <h1 className="w-full text-right text-h1 font-jost py-2 font-semibold ">Lost?</h1>
       <ul className="text-p1 font-bitter">
       {cats.map(cat => <li key={cat.category}>{cat.category}</li>)}
       </ul>

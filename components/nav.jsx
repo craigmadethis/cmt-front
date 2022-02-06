dc07f37e73a35393fa2bf93bb0458a5c4555daa8
Craig Shewry <craigshewry@gmail.com>
2022-02-06 15:17:47 +0000

    added lower border

--- components/nav.jsx
+++ components/nav.jsx
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 function Navbar() {
   return (
-	  <nav className='flex w-5/6 mx-auto pt-6 items-center'>
+	  <nav className='flex w-5/6 mx-auto pt-6 pb-4 items-center border-b-gray-200 border-b-2'>
     <div className="sm:w-1/3">
     <div className="flex flex-col text-h2 font-semibold font-jost">
     <a className=" hover:text-orange-400">photo</a>

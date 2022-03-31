03aeab6b592e4c5f35d6f0fc05b69be1d0dfe777
Craig Shewry <craigshewry@gmail.com>
2022-03-31 17:37:29 +0100

    restyled footer

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,7 +1,12 @@
 const Footer = () => {
   return (
-    <footer>
-    <h1 className="w-1/2 mx-auto text-center text-h3 font-jost font-semibold py-6">@CRAIGMADETHIS</h1>
+    <footer className="w-full bg-gray-900 text-gray-50">
+    <h1 className="w-1/2 mx-auto text-center text-h3 font-jost font-semibold py-6 ">@CRAIGMADETHIS</h1>
+    
+    <div className='w-full flex flex-col'>
+      <p className='w-full text-center'>if u need me don't call me</p>
+      <p className='w-full text-center'>craigshewry@gmail.com</p>
+    </div>
     <h1 className="w-1/2 mx-auto text-center text-p3 font-jost font-semibold py-6">© Craig Shewry 2022</h1>
     </footer>
 

196b8c0ee1c60c0f68e140195a3507a48a976541
Craig Shewry <craigshewry@gmail.com>
2022-04-01 11:23:39 +0100

    fixed routing

--- components/nav.jsx
+++ components/nav.jsx
@@ -4,9 +4,9 @@ function Navbar() {
 	  <nav className='flex w-full px-4 md:px-0 md:w-5/6 mx-auto pt-6 pb-4 items-center border-b-gray-200 border-b-2 justify-between'>
     <div className="sm:w-1/3">
     <div className="flex flex-col text-h2 font-semibold font-jost">
-    <h1 className="hover:text-orange-400" ><Link href='/'>photo</Link></h1>
-    <h1 className="hover:text-orange-400"> <Link className="hover:text-orange-400" href='/'>blog</Link></h1>
-    <h1 className="hover:text-orange-400"> <Link className="hover:text-orange-400" href='/'>me</Link></h1>
+    <h1 className="hover:text-orange-400" ><Link href='/blog/1'>photo</Link></h1>
+    <h1 className="hover:text-orange-400"> <Link className="hover:text-orange-400" href='/blog/1'>blog</Link></h1>
+    <h1 className="hover:text-orange-400"> <Link className="hover:text-orange-400" href='/blog/1'>me</Link></h1>
     </div>
     </div>
 	  <div className='text-d3 md:text-d2 font-bold font-jost whitespace-pre-wrap text-right w-2/3 hover:text-emerald-400 '>
--- next.config.js
+++ next.config.js
@@ -2,5 +2,19 @@ module.exports = {
   images: {
     domains: ["res.cloudinary.com", "localhost"],
   },
+  async redirects() {
+    return [
+      {
+      source: '/',
+      destination: '/blog/1',
+      permanent: true,
+      },
+      {
+      source: '/blog',
+      destination: '/blog/1',
+      permanent: true,
+      }
+    ]
+  }
 };
 

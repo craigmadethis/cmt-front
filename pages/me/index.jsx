d35d30f70320cf753f92b608994045473493ca78
Craig Shewry <craigshewry@gmail.com>
2022-04-03 18:44:59 +0100

    me

--- pages/me/index.jsx
+++ pages/me/index.jsx
@@ -13,17 +13,7 @@ const Me = (props) => {
     <ProseLayout>
     <div className='md col-span-8'>
     <h1 className='text-center'> hi im craig and I make things</h1>
-    <p> my name is craig and Im a photographer / videographer / retoucher / programmer / designer / physicist </p>
-
-    <p> Im not the chattiest person so Im just going to leave some links to some interesting things below.</p>
-    <p> On the photo page youll find some of my favourite photos of the moment. On the blog page, youll find out what Ive been up to including photoshoots and how I made things. </p>
-    <p>I like doing things the hard way so unlearnt my python skills and dove head first into react and nextjs to built this site. You can read how I designed the site in Figma here and how I built the site using nextjs, strapi and tailwindcss here </p>
-    <p>If youre interested in physics you can read my third, fourth and fifth year reports: 
-    <ul>
-    <li>Construction of a </li>
-    <li>The Higgs mechanism and naturalness</li>
-    <li>Construction of a </li>
-    </ul></p>
+    <p>wowow</p>
 
     </div>
     </ProseLayout>

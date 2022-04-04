b589bd5661b12c3ed97ba2099770a93d39ea4d95
Craig Shewry <craigshewry@gmail.com>
2022-04-04 18:51:53 +0100

    changing prose width

--- components/layouts.js
+++ components/layouts.js
@@ -23,7 +23,7 @@ export const ProseLayout = (props) => {
 	<div className='bg-gray-50 min-h-screen flex flex-col min-h-screen justify-between'>
     <Navbar />
 
-    <div className="w-full px-2 md:px-0 md:w-5/6 mx-auto grid grid-cols-8 md:grid-cols-12 mt-6 mb-auto ">
+    <div className="w-5/6 px-2 md:px-0 md:w-5/6 mx-auto grid grid-cols-8 md:grid-cols-12 mt-6 mb-auto ">
     <div className="col-span-12 grid grid-cols-8 border-b-2 border-gray-400">
     {props.children}
 

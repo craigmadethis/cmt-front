1e3495f06678812fa74a8b8af7a9f025beaf3a24
Craig Shewry <craigshewry@gmail.com>
2022-04-27 14:41:49 +0100

    post card changes: moved date to left, removed excess padding, added readmore link

--- components/postcard.jsx
+++ components/postcard.jsx
@@ -17,21 +17,22 @@ const PostCard = ({post}) => {
   return (
     <div key={postSlug} className="mr-4 mb-2 md:mb-4 col-span-4 border-b-4 border-blue-400 flex flex-l flex-col justify-between">
     <div key={postSlug}>
-      <h1 className="text-h3 md:text-h2 font-jost font-semibold hover:text-blue-400 w-full"><Link href='/blog/post/[slug]' as={`/blog/post/${postSlug}`}>{postTitle}</Link></h1>
+      <Link href='/blog/post/[slug]' as={`/blog/post/${postSlug}`}><a><h1 className='text-h3 md:text-h2 font-jost font-semibold hover:text-blue-400 w-full pb-2 '>{postTitle}</h1></a></Link>
     </div>
       <div className='relative aspect-[4/3] w-full max-h-64 mt-2 place-self-center '>
         <Image src={`${coverUrl}`} layout='fill' objectFit='cover' />
       </div>
       {/* <div className="w-5/6 bg-emerald-400 h-[200px]"> an image </div> */}
     <div>
-      <div className='w-full content-end font-bitter text-p3 md:text-p2 py-2 md:py-4'>
+      <div className='w-full content-end font-bitter text-p3 md:text-p2 pt-2 md:pt-4'>
         <TruncateMarkup lines={6}>
         <div>
           <p>{postDescription}</p>
+<Link href='/blog/post/[slug]' as={`/blog/post/${postSlug}`}><a><p className='w-full pt-2 font-jost font-semibold  text-p2 md:text-p1 text-right hover:text-blue-400'>Read more {'>'}</p></a></Link>
         </div>
         </TruncateMarkup>
       </div>
-      <p className='pt-2 w-full text-right text-p3 font-jost font-bold place-self-end'>{postDate.toLocaleDateString()}</p>
+      <p className='pt-2 w-full text-left text-p3 font-jost font-bold place-self-end'>{postDate.toLocaleDateString()}</p>
     </div>
     </div>
   )

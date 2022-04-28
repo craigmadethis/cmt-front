fc756dd84f8637f44a264248490cd13cbbb41a75
Craig Shewry <craigshewry@gmail.com>
2022-04-28 22:11:21 +0100

    changing min height of prose on me page etc to fit content to remove large space at bottom

--- components/layouts.js
+++ components/layouts.js
@@ -24,7 +24,7 @@ export const ProseLayout = (props) => {
     <Navbar />
 
     <div className="w-full px-2 md:px-0 md:w-5/6 mx-auto grid grid-cols-8 md:grid-cols-12 mt-6 mb-auto ">
-    <div className="col-span-12 grid grid-cols-8 border-b-2 border-gray-400">
+    <div className="col-span-12 grid grid-cols-8 border-b-2 border-gray-200">
     {props.children}
 
 

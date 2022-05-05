2c29f42b9a9536c567a8a464fb2c296fd171da8c
Craig Shewry <craigshewry@gmail.com>
2022-05-05 12:11:54 +0100

    changes to postgrid to make everything line up

--- components/postcard.jsx
+++ components/postcard.jsx
@@ -12,24 +12,24 @@ const PostCard = ({post}) => {
   const postDate = new Date(postCreated)
   return (
 <Link href='/blog/post/[slug]' as={`/blog/post/${postSlug}`} key={postSlug}>
-    <a  className="mr-4 mb-2 md:mb-4 col-span-4 border-b-4 border-blue-400 flex flex-l flex-col justify-between h-[60vh]">
+    <a  className="mr-4 mb-2 md:mb-4 col-span-4 border-b-4 border-blue-400 flex flex-col max-h-[70vh]">
     <div key={postSlug}>
-      <Link href='/blog/post/[slug]' as={`/blog/post/${postSlug}`}><a><h1 className='text-h2 font-jost font-semibold hover:text-blue-400 w-full pb-2 '>{postTitle}</h1></a></Link>
+      <Link href='/blog/post/[slug]' as={`/blog/post/${postSlug}`}><a><h1 className='h-24 text-h2 font-jost font-semibold hover:text-blue-400 w-full pb-2 '>{postTitle}</h1></a></Link>
     </div>
-      <div className='relative aspect-[4/3] w-full max-h-64 mt-2 place-self-center '>
+      <div className='relative w-full aspect-square mt-2 place-self-center m-6 '>
         <Image src={`${coverUrl}`} layout='fill' objectFit='cover' priority='true' sizes='50vw' placeholder='blur' blurDataURL={blurLoader(coverUrl)}/>
       </div>
       {/* <div className="w-5/6 bg-emerald-400 h-[200px]"> an image </div> */}
     <div>
-      <div className='w-full content-end font-bitter text-p3 md:text-p2 pt-2 md:pt-4'>
-        <TruncateMarkup lines={6}>
-        <div>
+      <div className='w-full font-bitter text-p3 md:text-p2 pt-2 md:pt-4 h-32'>
+        <TruncateMarkup lines={6} >
+        <div >
           <p>{postDescription}</p>
         </div>
         </TruncateMarkup>
-    <Link href='/blog/post/[slug]' as={`/blog/post/${postSlug}`}><a><p className='w-full pt-2 font-jost font-semibold  text-p2 md:text-p1 text-right hover:text-blue-400'>Read more {'>'}</p></a></Link>
       </div>
-      <p className='pt-2 w-full text-left text-p3 font-jost font-bold place-self-end'>{postDate.toLocaleDateString()}</p>
+    <Link href='/blog/post/[slug]' as={`/blog/post/${postSlug}`}><a><p className='w-full pt-2 font-jost font-semibold  text-p2 md:text-p1 text-right hover:text-blue-400 justify-end'>Read more {'>'}</p></a></Link>
+      <p className='pt-2 w-full text-left text-p3 font-jost font-bold justify-end'>{postDate.toLocaleDateString()}</p>
     </div>
     </a></Link>
   )

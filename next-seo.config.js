5e530f9dffdc6bb5a74884d98da251e385c90ae2
Craig Shewry <craigshewry@gmail.com>
2022-05-15 19:45:55 +0100

    changing some seo params

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,13 +1,13 @@
 export default {
-  defaultTitle: '@CRAIGMADETHIS',
-  titleTemplate:'%s | @CRAIGMADETHIS',
-  description: 'Documentary and music photographer based in Glasgow, Scotland. @craigmadethis.',
+  defaultTitle: 'CRAIGMADETHIS',
+  titleTemplate:'%s | CRAIGMADETHIS',
+  description: "CRAIGMADETHIS (Craig Shewry) is a documentary photographer and music photographer from Glasgow, Scotland. Here you will find both Craig's blog and Craig's photography portfolio.",
   canonical: 'https://craigmadethis.com',
     openGraph: {
         type: 'website', 
         locale: 'en_GB', 
         url: 'https://craigmadethis.com/',
-        site_name: '@CRAIGMADETHIS',
+        site_name: 'CRAIGMADETHIS',
       },
       twitter: {
           handle: '@craigmadethis',

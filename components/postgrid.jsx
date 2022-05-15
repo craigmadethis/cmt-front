4c3d5658e8988a17e45cb4f437d8974ba318cf08
Craig Shewry <craigshewry@gmail.com>
2022-05-15 17:56:33 +0100

    added 'posts' title to posts page

--- components/postgrid.jsx
+++ components/postgrid.jsx
@@ -5,6 +5,7 @@ const PostGrid = ({posts, pagination, full }) => {
   if (full) {
     return (
       <>
+      <h1 className="mx-2 mb-4 text-orange-400 col-span-8 text-h1 font-jost font-semibold">Posts</h1>
       <main className="col-span-8 md:col-span-9 grid grid-cols-8 md:grid-cols-12 items-stretch" >
       <div>
       {posts.map(post => <PostCard post={post} key={post.id} />)} 
@@ -17,6 +18,7 @@ const PostGrid = ({posts, pagination, full }) => {
   else {
     return(
       <>
+      <h1 className="mx-2 mb-4 text-orange-400 col-span-8 text-h1 font-jost font-semibold">Posts</h1>
       <main className="col-span-8 md:col-span-9 grid grid-cols-4 md:grid-cols-8 items-stretch" >
       {posts.map(post => <PostCard post={post} key={post.id} />)} 
       <Pagination pageCount={pageCount}/>

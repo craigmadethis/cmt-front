457e31b8b183e2467ee7440a057f0efaa6ee742e
Craig Shewry <craigshewry@gmail.com>
2022-05-18 17:43:16 +0100

    reverting

--- components/layouts.js
+++ components/layouts.js
@@ -55,11 +55,11 @@ export const GalleryLayout = (props) => {
     <div className='flex flex-col md:flex-row md:flex-wrap justify-center items-center'>
     {allImages.map(({attributes: {url, caption, width, height, alternativeText: alt}}) => 
       <Link href='' key={url}>
-          <a>
+          <a className='' href="javascript:;">
           <LightgalleryItem src={url} group="page" subHtml={caption} className='aspect-square p-2' >
       {/* <img className="object-contain max-h-[50vh] md:max-h-[40vh] p-1" src={`${url}`} alt={alt}/> */}
       <div className='h-[40vh] aspect-square m-1 hover:opacity-75 relative'>
-      <Image className="" src={`${url}`} alt={`${alt}`} layout="fill"  sizes="80vw" objectFit='cover' placeholder='blur' blurDataURL={blurLoader(url)}/>
+      <Image className="" src={`${url}`} alt={`${alt}`} layout="fill"  sizes="100vw" objectFit='cover' placeholder='blur' blurDataURL={blurLoader(url)}/>
       </div>
       </LightgalleryItem>
       </a>

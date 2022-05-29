7e355fa7dd2a3e8880e5480b4ce8f84e9215f930
Craig Shewry <craigshewry@gmail.com>
2022-05-29 20:32:22 +0100

    Revert "small things"
    This reverts commit 8b87241961448b6dbb1a938f06a8a878082ca5c2.

--- pages/blog/[id].jsx
+++ pages/blog/[id].jsx
@@ -1,69 +1,69 @@
 // import Image from 'next/image'
-import { gql } from '@apollo/client'
-import { SidebarLayout } from '../../components/layouts'
+import {gql} from '@apollo/client'
+import {SidebarLayout} from '../../components/layouts'
 import PostGrid from '../../components/postgrid'
 import InitClient from '../../lib/client'
 import postperpage from '../../lib/postperpage'
-import { POST_LIST, PAGE_DATA, GET_SOCIALS } from '../../lib/queries'
-import { useRouter, withRouter } from 'next/router'
+import {POST_LIST, PAGE_DATA, GET_SOCIALS} from '../../lib/queries'
+import{ useRouter, withRouter } from 'next/router'
 
 
 const Home = (props) => {
-  let { posts, categories, pagination, socials } = props
+  let {posts, categories, pagination, socials} = props
 
 
-  return (
-    <>
-      <SidebarLayout categories={categories} socials={socials}>
-        <PostGrid posts={posts} pagination={pagination} full={false} />
-      </SidebarLayout>
-    </>
+return  (
+  <>
+    <SidebarLayout categories={categories} socials={socials}>
+      <PostGrid posts={posts} pagination ={pagination} full={false} />
+    </SidebarLayout>
+  </>
 
   )
 }
 
 export default withRouter(Home);
 
-export const getStaticProps = async ({ params }) => {
+export const getStaticProps = async ({params}) => {
   const client = InitClient()
-  let { id } = params
-  const { data: {
+  let {id} = params
+  const {data:{
     posts: {
-      data: postsData,
+      data: postsData, 
       meta: {
-        pagination: pagination }
-    },
+        pagination: pagination}
+    }, 
     categories: {
-      data: catsData }
+      data: catsData}
   }
   } = await client.query(
     {
-      query: gql(POST_LIST),
-      variables: { pageNum: parseInt(id), size: postperpage }
+    query: gql(POST_LIST),
+    variables:{pageNum:parseInt(id), size:postperpage}
     }
   )
 
-  const { data: { footer: { data: { attributes: { socials: socialsData } } } } } = await client.query(
-    { query: gql(GET_SOCIALS) }
+  const {data:{footer: {data: {attributes: {socials: socialsData}} }}} = await client.query(
+    {query: gql(GET_SOCIALS)}
   )
 
-  return {
-    props: {
-      posts: postsData,
-      categories: catsData,
-      pagination: pagination,
-      socials: socialsData,
+    return {
+        props: {
+            posts: postsData,
+            categories: catsData,
+            pagination: pagination,
+          socials: socialsData,
+        }
     }
-  }
 }
 
 export const getStaticPaths = async () => {
 
   const client = InitClient()
-  const { data: { posts: { meta: { pagination: { pageCount: totalPages } } } } } = await client.query(
+  const {data: {posts:{meta: {pagination: {pageCount: totalPages}}}}} = await client.query(
     {
-      query: gql(PAGE_DATA),
-      variables: { size: 1 },
+    query: gql(PAGE_DATA),
+    variables:{size:1},
     }
   )
 
@@ -74,17 +74,17 @@ export const getStaticPaths = async () => {
   //   pageIds.push(`/blog/${i}`)
   // }
 
-  let pageIds = []
-  for (let i = 1; i <= totalPages; i++) {
+  let pageIds=[]
+  for (let i = 1; i<=totalPages; i++){
     pageIds.push(
-      { params: { id: `${i}` } }
+      {params:{id: `${i}`}}
     )
   }
 
   return {
     // paths: ['/blog/1', '/blog/2'],
     paths: pageIds,
-    fallback: false,
+    fallback:false,
   }
 
 
